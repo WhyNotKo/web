4f3d35a8b931cf951584883847facfb38269af65
WhyNotKo <112894857+WhyNotKo@users.noreply.github.com>
2024-09-10 23:12:26 +0300

    Part4
    Add a model

--- MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f3eabe0700f8b68f59231211412880c253e2d08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07f2fd2930aaa3f5e3d111600ee83a3cef5d6806")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
