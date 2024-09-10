456046a5808ed4108337ca350b9a31b393fd959c
WhyNotKo <112894857+WhyNotKo@users.noreply.github.com>
2024-09-11 02:46:45 +0300

    Part5
    Work with database

--- MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07f2fd2930aaa3f5e3d111600ee83a3cef5d6806")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f3d35a8b931cf951584883847facfb38269af65")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
